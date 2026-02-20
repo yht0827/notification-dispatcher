c7e43e7c5cb092c26eef4a33680adac84005beb9
yht0827 <yangheetae0827@naver.com>
2026-02-20 23:12:03 +0900

    initial commit

---
+++ src/main/java/com/example/notificationdispatcher/NotificationDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationdispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationDispatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notificationdispatcher/NotificationDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationdispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
